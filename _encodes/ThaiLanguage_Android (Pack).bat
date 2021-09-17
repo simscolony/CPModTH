50dba967ba9fe876a76d3654300f308698cb8d32
ELL <ellipszist@gmail.com>
2021-09-17 12:41:08 +0700

    ลบไฟล์ unpack ที่ไม่ได้ใช้งานออก

--- _encodes/ThaiLanguage_Android (Pack).bat
+++ _encodes/ThaiLanguage_Android (Pack).bat
@@ -1,4 +1,4 @@
-xnbcli-windows-x64\xnbcli.exe pack ".\ThaiLanguage_Android\assets\Content" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\.AndroidModTH\assets\Content"
+_xnbcli\xnbcli.exe pack ".\ThaiLanguage_Android\assets\Content" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\.AndroidModTH\assets\Content"
 COPY ".\ThaiLanguage_Android\assets\Content\Data\Concessions.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\.AndroidModTH\assets\Content\Data\Concessions.json"
 COPY ".\ThaiLanguage_Android\assets\Content\Data\Movies.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\.AndroidModTH\assets\Content\Data\Movies.json"
 COPY ".\ThaiLanguage_Android\assets\Content\Data\MoviesReactions.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\.AndroidModTH\assets\Content\Data\MoviesReactions.json"
