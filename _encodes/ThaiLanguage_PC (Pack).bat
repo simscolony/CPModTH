955af6d9f035bf5758849a7e661a479e54adb5a3
ELL <ellipszist@gmail.com>
2021-09-22 14:31:54 +0700

    Revert "Create .gitignore"
    This reverts commit 7b9f15e158c467ce2071191c2a63ca217a0b22f9.

--- _encodes/ThaiLanguage_PC (Pack).bat
+++
@@ -1,6 +0,0 @@
-_xnbcli\xnbcli.exe pack ".\ThaiLanguage_PC\assets\Content" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\[CP] ModTH\assets\Content"
-COPY ".\ThaiLanguage_PC\assets\Content\Data\Concessions.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\[CP] ModTH\assets\Content\Data\Concessions.json"
-COPY ".\ThaiLanguage_PC\assets\Content\Data\Movies.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\[CP] ModTH\assets\Content\Data\Movies.json"
-COPY ".\ThaiLanguage_PC\assets\Content\Data\MoviesReactions.json" "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\[CP] ModTH\assets\Content\Data\MoviesReactions.json"
-echo Press enter to continue
-pause
